bfc6c52161dedb15fc7c1ed77f61021d9b1872e8
mikefallon81 <mikefallon81@gmail.com>
2021-07-20 17:50:39 +0100

    react router installed, nav component created

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Header';
+import Navigation from './Navigation';
 
 class App extends React.Component {
     
     render(){
-        return <Header/>
+        return <>
+        <Header/>
+        <Navigation/>
+        </>
     }
 }
 export default App;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Navigation extends React.Component{
+
+    render(){
+        return <div>Navigation</div>
+    }
+}
+
+export default Navigation;
